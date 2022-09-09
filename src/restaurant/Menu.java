8e5607c17bd0745a0e2f7e63650459c94cba276e
Matt McKeever <mmckeever23@gmail.com>
2022-09-08 19:26:31 -0500

    Commit 1

---
+++ src/restaurant/Menu.java
@@ -0,0 +1,9 @@
+package restaurant;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Menu {
+    public ArrayList<MenuItem> MenuItems;
+    public Date LastUpdated;
+}
---
+++ src/restaurant/MenuItem.java
@@ -0,0 +1,40 @@
+package restaurant;
+
+public class MenuItem {
+    private double price;
+    private String description;
+    private String category;
+    private boolean isNew;
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isNew() {
+        return isNew;
+    }
+
+    public void setNew(boolean aNew) {
+        isNew = aNew;
+    }
+}
